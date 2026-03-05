9f60635fe82ad3ecb9462347b0fd85d30bc345d1
wcv007 <varunsingla2007@gmail.com>
2026-03-05 23:23:00 +0530

    spotify-better change bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -65,10 +65,13 @@ app.use("/api/songs", songRoutes);
 app.use("/api/stats", statRoutes);
 app.use("/api/users", userRoutes);
 
+const distPath = path.resolve(__dirname, "../frontend/dist");
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend", "dist")));
+  app.use(express.static(distPath));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(distPath, "index.html"));
   });
 }
 
